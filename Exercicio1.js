580cced07dcb18da22bcc66dbe08fe1827e86468
Carlos Bellete <dcarlosbellete@gmail.com>
2025-04-08 14:30:42 -0300

    Exercicio introdutório de maniulação de strings em java script

---
+++ Exercicio1.js
@@ -0,0 +1,9 @@
+const nome = "Malu"
+let idade = 7
+let peso = 10
+const raca = "vira-lata"
+let adotado = true
+let nomeMaiuscula = nome.toUpperCase()
+let corecao = raca.slice(0, 1).toUpperCase() + raca.slice(1).toLowerCase()
+
+let tag = console.log(`Nome: ${nomeMaiuscula} | idade: ${idade} | Peso: ${peso} | Raça: ${corecao}`)
