db5a10b9615f54c5a0b2d1cc6a5607e307c9aada
Carlos Bellete <dcarlosbellete@gmail.com>
2025-04-14 12:22:46 -0300

    Inclusão de condicionais if e else, contabilizando a quantidade de catacteres com a propriedade length

--- Exercicio3.js
+++ Exercicio3.js
@@ -7,3 +7,9 @@ const dog2Padrao = dog2SemEspaco.slice(0,1).toUpperCase() + dog2SemEspaco.slice(
 
 console.log(`O nome do dog 1 da maneira que foi mencionado é ${dog1} e após formatação é ${dog1Padrao}`)
 console.log(`O nome do dog 1 da maneira que foi mencionado é ${dog2} e após formatação é ${dog2Padrao}`)
+
+if(dog1Padrao.length == 4 && dog2Padrao.length == 5){
+    console.log(`O nome ${dog1Padrao}, contém apenas ${dog1Padrao.length} letras e o nome ${dog2Padrao}, contém apens ${dog2Padrao.length} caracteres`)
+}else{
+    console.log(`O nome ${dog1Padrao}, contém a quantidade de caracteres diferente de ${dog1Padrao.length} e o nome ${dog2Padrao}, contém a quantidade de caracteres diferente de ${dog2Padrao.length} caracteres`)
+}
