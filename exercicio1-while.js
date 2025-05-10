ed4a7fe8e1465d6eed871d8880276f9b4c3724b7
Carlos Bellete <dcarlosbellete@gmail.com>
2025-05-10 09:09:29 -0300

    estrutura de repetição

---
+++ exercicio1-while.js
@@ -0,0 +1,14 @@
+function validarSatisfacaoCachorro(){
+
+    let voltasADar = 5
+    let inicioVoltas = 1
+
+    while(inicioVoltas <= voltasADar){
+    console.log(`A volta atual é ${inicioVoltas}`)
+    inicioVoltas++
+    }   
+
+    console.log("O dog já está satisfeito.")
+}
+
+validarSatisfacaoCachorro()
